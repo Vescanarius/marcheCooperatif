710dae74988ff4e0ea0dc4453dbb9298b6f01bde
Thomas <gudinthomas@gmail.com>
2019-04-17 15:30:34 -0400

    Ajout d'un service de paniers

--- src/app/caisse/caisse.module.ts
+++ src/app/caisse/caisse.module.ts
@@ -5,7 +5,6 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
 import { CaissePage } from './caisse.page';
-import { PanierPage } from './panier/panier.page';
 
 
 import { MarcheSearch } from './scan-client.component';
