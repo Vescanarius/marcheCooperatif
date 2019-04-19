8dea1bc68f36a23829d7c3a5dbf8255beb32c094
Thomas <gudinthomas@gmail.com>
2019-04-19 17:27:17 -0400

    debug

--- src/app/caisse/panier/add-product/add-product.component.ts
+++ src/app/caisse/panier/add-product/add-product.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'add-product',
     templateUrl: 'add-product.component.html',
     styleUrls: ['add-product.component.scss']
