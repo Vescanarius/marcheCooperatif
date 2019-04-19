26c1234c4a540fc467dba3dab248baf007d634c9
Thomas <gudinthomas@gmail.com>
2019-04-19 11:42:48 -0400

    Nouveau composant add product

---
+++ src/app/caisse/panier/add-product-to-panier/add-product-to-panier.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector: 'add-product-to-panier',
+    templateUrl: 'add-product-to-panier.component.html',
+    styleUrls: ['add-product-to-panier.component.scss']
+})
+export class AddProductToPanierComponent {
+
+
+    addProductToPanier(){}
+
+}
---
+++ src/app/caisse/panier/add-product-to-panier/add-product-to-panier.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { AddProductToPanierComponent } from './add-product-to-panier.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        AddProductToPanierComponent,
+    ],
+    exports: [
+        AddProductToPanierComponent,
+    ]
+})
+export class AddProductToPanierModule {
+
+}
